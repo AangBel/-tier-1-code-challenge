a7581e7ce76bfe8874307c385b2b96cacecf3335
Angelica Beltran <angelica.beltran11@gmail.com>
2023-05-01 16:56:53 -0500

    got up to number 6 so far

--- client.js
+++ client.js
@@ -6,13 +6,13 @@ You’ve been given an index.html file and this starter js file.
 */
 
 console.log('Step 0 complete! client.js loaded!');
-            
+
 // Write your code here!
 
 // 1. Start with the number 29 and set that equal to a variable named ‘highScore’;
-
+let highScore = 29;
 // 2. Make a new empty array called myScrabbleTiles.
-
+let myScrabbleTiles = [];
 // 3. Put these 6 tile objects into myScrabbleTiles:
 /*
   { tile: 'N', score: 1 }
@@ -23,19 +23,33 @@ console.log('Step 0 complete! client.js loaded!');
   { tile: 'A', score: 1 }
 */
 
+myScrabbleTiles.push(
+  { tile: 'N', score: 1 },
+  { tile: 'K', score: 5 },
+  { tile: 'Z', score: 10 },
+  { tile: 'X', score: 8 },
+  { tile: 'D', score: 2 },
+  { tile: 'A', score: 1 },
+);
+console.log(myScrabbleTiles);
 // 4. Remove the last tile from myScrabbleTiles.
 
-
+myScrabbleTiles.pop();
 // 5. Add the following new tile to myScrabbleTiles:
 /*
   { tile: 'F', score : 4 }
 */
-
-// 6. Complete this function. It needs to be given an array of tile objects. The function will use a for-loop and return the sum of all the scores in the given array.
-function sumTiles(anArray) {
-
+myScrabbleTiles.push(
+  { tile: 'F', score: 4 }
+);
+//console.log(myScrabbleTiles);
+// 6. Complete this function. It needs to be given an array of tile objects. 
+//The function will use a for-loop and return the sum of all the scores in the given array.
+function sumTiles(myScrabbleTiles) {
+  for (let i= 0; i < myScrabbleTiles.length; i++) {
+    console.log(sum += myScrabbleTiles[i]);
 }
-
+}
 
 // 7. Use the function above to get the total score for myScrabbleTiles and assign the value to a variable called "myScore".
 
